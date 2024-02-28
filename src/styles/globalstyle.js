51ac2f9d0eab8892615108575d758c3814245370
Clarissa de Cassia <clari.developer@gmail.com>
2024-02-28 17:12:57 -0300

    :construction: add global style

---
+++ src/styles/globalstyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    font-weight: 300;
+    box-sizing: border-box;
+
+    body{
+      
+    }
+    
+  }
+  `
