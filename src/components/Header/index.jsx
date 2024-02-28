a2a0b4feadaac8625960ec124a76f1b6f4e5040b
Clarissa de Cassia <clari.developer@gmail.com>
2024-02-28 12:22:00 -0300

    :construction: add Toggler

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,19 @@
 import { HeaderContainer, LogoContainer } from "./styles"
 import { LogoPokemon } from "../LogoPokemon/index"
-import {LogoPokebola} from "../LogoPokebola/index"
-
+import { LogoPokebola } from "../LogoPokebola/index"
+import { Toggler } from "../Toggler"
 
 export const Header = () => {
     return (
         <HeaderContainer>
+
+            <Toggler></Toggler>
+
             <LogoContainer>
                 <LogoPokemon></LogoPokemon>
                 <LogoPokebola></LogoPokebola>
             </LogoContainer>
+
         </HeaderContainer>
     )
 }
---
+++ src/components/Toggler/index.jsx
@@ -0,0 +1,13 @@
+import{ useTheme } from './../../hooks/useTheme'
+
+export const Toggler = () => {
+
+     const { theme, setTheme } = useTheme()  
+
+     console.log("🚀 ~ Toggler ~ theme:", theme)
+
+    return (
+        <></>
+    )
+
+}
