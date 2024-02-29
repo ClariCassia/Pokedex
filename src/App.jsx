18665332116ba1f80dc59049cb5827b5e9d765f6
Clarissa de Cassia <clari.developer@gmail.com>
2024-02-29 11:01:37 -0300

    :construction: Add rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 import { useTheme } from "./hooks/useTheme"
+import Rotas from "./components/routes";
 
 function App() { 
 
   const { theme } = useTheme();  
 
-  return <ContainerTheme theme = {theme}/> 
+  return (
+   <ContainerTheme theme={theme}>
+    
+    <Rotas/>
+
+   </ContainerTheme>
+  )
      
 }
 
 const ContainerTheme = styled.div`
 background-color: ${props=> props.theme.background};
+color: ${props=> props.theme.color};
 min-height: calc(100vh - 190px);
 
 `
---
+++ src/components/routes/index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react-dom/client';
+import Home from '../../pages/Home'
+import Details from '../../pages/Details'
+
+
+const Rotas = ()=>{
+    return (
+        <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/Details" element={<Details/>}></Route>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Rotas
