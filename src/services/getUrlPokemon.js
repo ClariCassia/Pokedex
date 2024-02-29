e0cbc59a652d42b48442d84d35286ac466e6c3ed
Clarissa de Cassia <clari.developer@gmail.com>
2024-02-29 11:22:28 -0300

    :construction: add getUrlPokemon

---
+++ src/services/getUrlPokemon.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const getList = async () => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10`)
+
+    const { results } = response.data
+
+    let urlPokemon = results.map((result) => result.url)
+
+    return await Promise.all(
+        urlPokemon.map((url) => axios.get(url)
+            .then((response) => response.data))
+    )
+}
