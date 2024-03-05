e9e04c4705a97d0112d23c0f6daf5149e2ae3af5
Clarissa de Cassia <clari.developer@gmail.com>
2024-03-05 11:41:57 -0300

    :construction: add responsividade

--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -1,47 +1,59 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ContainerCard = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-flex-wrap: wrap;
-max-width: 1024px;
-gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 1180px;
+  gap: 15px;
+  padding: 50px;
+  background-color: yellow;
 
-@media (max-width: 768px) {   
+  @media (max-width: 1024px) and (min-width: 800px) {
     padding: 20px;
-    max-width: 500px;    
+    justify-content: flex-start;
   }
-`
+
+  @media (max-width: 768px) {
+    padding: 20px;
+    max-width: 600px;
+  }
+
+  @media (max-width: 568px) {
+    max-width: 400px;
+  }
+`;
+
 export const ContainerHome = styled.div`
-display: flex;
-flex-direction:column;
-align-items: center;
-justify-content: center;
-
-
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const typeColors = {
-  grass: '#8BBE8A',
-  fire: '#FFA756',
-  water: '#58ABF6',
-  poison: '#9F6E97',
-  normal: '#B5B9C4',
-  bug: '#8BD674',
-  flying: '#748FC9',
-  eletric: '#F2CB55',
-  ground: '#F78551',
+  grass: "#8BBE8A",
+  fire: "#FFA756",
+  water: "#58ABF6",
+  poison: "#9F6E97",
+  normal: "#B5B9C4",
+  bug: "#8BD674",
+  flying: "#748FC9",
+  eletric: "#F2CB55",
+  ground: "#F78551"
+  // Adicione outros tipos, se necessário
 };
 
 export const colorsLabel = {
-  grass: '#5b8f56',
-  fire: '#e97110',
-  water: '#3a86c0',
-  poison: '#6e4b6f',
-  normal: '#7a7d88',
-  bug: '#5b8f43',
-  flying: '#4f6d99',
-  eletric: '#b18e39',
-  ground: '#b05c2c',
+  grass: "#5b8f56",
+  fire: "#e97110",
+  water: "#3a86c0",
+  poison: "#6e4b6f",
+  normal: "#7a7d88",
+  bug: "#5b8f43",
+  flying: "#4f6d99",
+  eletric: "#b18e39",
+  ground: "#b05c2c"
+  // Adicione outros tipos, se necessário
 };
