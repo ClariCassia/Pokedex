66b9be43a89bb89e8de91a42071dd1b00bda7f57
Clarissa de Cassia <clari.developer@gmail.com>
2024-03-08 18:52:24 -0300

    :construction: add arquivo styles PokemonDetails

--- src/components/PokemonDetails/index.jsx
+++ src/components/PokemonDetails/index.jsx
@@ -1,223 +1,83 @@
 import React from 'react';
-import {StyledArrowLeft} from './styles'
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { Container,ContainerDescription,ProgressBarContainer,ProgressBar,BackButton ,Div} from './styles';
+
 import { useTheme } from '../../hooks/useTheme';
-import firstLetterUppercase from '../../services/firstLetterUppercase'
-import {getTypeColor,getTypeColorLabel} from '../../services/getTypeColors'
+import { firstLetterUppercase } from '../../services/firstLetterUppercase';
+import { getTypeColor, getTypeColorLabel } from '../../services/getTypeColors';
+import { StyledArrowLeft } from './styles';
 
 const PokemonDetails = ({ pokemonInfo }) => {
-
   const navigate = useNavigate();
-
   const { theme } = useTheme();
 
   if (!pokemonInfo) {
     return <div>Loading...</div>;
   }
 
-  let malePercentage = 0;
-  let femalePercentage = 0;
+  const { gender, types, eggGroups, height, pokemonWeight, abilities, weaknesses, evolutionLine, nome, imagem } = pokemonInfo;
 
-  // Se o gênero não for 'Genderless', calcula as porcentagens
-  if (pokemonInfo.gender !== 'Genderless') {
-    const [maleString, femaleString] = pokemonInfo.gender.split(', ');
-
-    // Verifica se maleString e femaleString são válidos antes de calcular as porcentagens
-    if (maleString && femaleString) {
-      malePercentage = parseFloat(maleString.match(/\d+/)[0]);
-      femalePercentage = parseFloat(femaleString.match(/\d+/)[0]);
-    }
-  }
+  const [malePercentage, femalePercentage] = gender !== 'Genderless' ? pokemonInfo.gender.split(', ').map(genderStr => parseFloat(genderStr.match(/\d+/)[0])) : [0, 0];
 
   return (
-    <>
-      <Container>
-        <div>
-          <BackButton onClick={() => navigate(-1)}>
-          <StyledArrowLeft theme={theme}  />  
-          </BackButton>
-          <img src={pokemonInfo.imagem} alt="" />
-        </div>
+    <Container>
+      <BackButton onClick={() => navigate(-1)}>
+        <StyledArrowLeft theme={theme} />
+      </BackButton>
 
-        <ContainerDescription theme={theme} background={getTypeColor(pokemonInfo.types[0])} color={getTypeColorLabel(pokemonInfo.types[0])}>
-          <h2>{pokemonInfo.nome.charAt(0).toUpperCase() + pokemonInfo.nome.slice(1)}</h2>
+      <img src={imagem} alt="Imagem Pokemon" />
 
-          <Teste>
-            <span >Egg Groups:</span>
-            <p>{pokemonInfo.eggGroups.map(group => firstLetterUppercase(group)).join(", ")}</p>
+      <ContainerDescription theme={theme} background={getTypeColor(types[0])} color={getTypeColorLabel(types[0])}>
+        <h2>{firstLetterUppercase(nome)}</h2>
 
-            <span>Type:</span>
-            <p>{pokemonInfo.types.map(type => firstLetterUppercase(type)).join(", ")}</p>
+        <Div>
+          <span>Egg Groups:</span>
+          <p>{eggGroups.map(group => firstLetterUppercase(group)).join(", ")}</p>
 
-          </Teste>
+          <span>Type:</span>
+          <p>{types.map(type => firstLetterUppercase(type)).join(", ")}</p>
+        </Div>
 
-          <span>Gender:</span>
-          {pokemonInfo.gender === 'Genderless' ? (
-            <p>Sem especificação de gênero</p>
-          ) : (
-            <ProgressBarContainer>
-              <div>Masculino:</div>
-              <ProgressBar color="#105be7b5" width={malePercentage}>
-                {malePercentage}%
-              </ProgressBar>
-              <div>Feminino:</div>
-              <ProgressBar color="#ff0783" width={femalePercentage}>
-                {femalePercentage}%
-              </ProgressBar>
-            </ProgressBarContainer>
-          )}
+        <span>Gender:</span>
+        {gender === 'Genderless' ? (
+          <p>Sem especificação de gênero</p>
+        ) : (
+          <ProgressBarContainer>
+            <p>Masculino:</p>
+            <ProgressBar color="#105be7b5" width={malePercentage}>
+              {malePercentage}%
+            </ProgressBar>
+            <p>Feminino:</p>
+            <ProgressBar color="#ff0783" width={femalePercentage}>
+              {femalePercentage}%
+            </ProgressBar>
+          </ProgressBarContainer>
+        )}
 
-          <Teste>
-            <span>Height:</span>
-            <p> {pokemonInfo.height} m</p>
-            <span> Weight:</span>
-            <p> {pokemonInfo.pokemonWeight} kg</p>
-          </Teste>
+        <Div>
+          <span>Height:</span>
+          {height < 1 ? <p>{height} cm</p> : <p>{height} metros </p>}
+          
 
-          <span>Abilities:</span>
-          <p>{pokemonInfo.abilities.map(abilitie => firstLetterUppercase(abilitie)).join(", ")}</p>
+          <span> Weight:</span>
+          <p>{pokemonWeight} kg</p>
+        </Div>
 
-          <span>Weaknesses:</span>
-          <p>{pokemonInfo.weaknesses.map(weaknesse => firstLetterUppercase(weaknesse)).join(", ")}</p>
+        <span>Abilities:</span>
+        <p>{abilities.map(ability => firstLetterUppercase(ability)).join(", ")}</p>
 
+        <span>Weaknesses:</span>
+        <p>{weaknesses.map(weakness => firstLetterUppercase(weakness)).join(", ")}</p>
 
-          <span>Evolution Line:</span>
-          <ul>
-            {pokemonInfo.evolutionLine.map((pokemon, index) => (
-              <li key={index}>{firstLetterUppercase(pokemon)}</li>
-            ))}
-          </ul>
-        </ContainerDescription>
-      </Container>
-    </>
+        <span>Evolution Line:</span>
+        <ul>
+          {evolutionLine.map((pokemon, index) => (
+            <li key={index}>{firstLetterUppercase(pokemon)}</li>
+          ))}
+        </ul>
+      </ContainerDescription>
+    </Container>
   );
 };
 
 export default PokemonDetails;
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  max-width: 1180px;
-  gap: 15px;
- 
-
-
-  @media (max-width: 768px) {
-    font-size: 12px;
-    flex-direction: column;
-    padding: 20px;   
-   
-    img{
-      width: 250px;
-    }
-  } 
-  
-`;
-
-const ContainerDescription = styled.div`
-  display: flex;
-  flex-direction: column; 
- 
-  gap: 20px;
-  min-width: 500px;
-  padding: 20px;
-  box-shadow: ${props => props.theme.boxShadow};
-  background-color: ${props => props.background};
-  
-
-  h2 {
-    font-size: 40px;
-    color: aliceblue;
-    background-color: ${props => props.color};
-    padding:10px;
-    width:100%;
-    text-align:center;
-    border-radius:5px
-  }
-
-  span{
-    background-color: ${props => props.color};
-    padding: 5px 15px;
-    color: white;
-    text-align: center;
-  } 
-
-  ul{
-    display: flex;
-    justify-content: space-around;
-    list-style: none;
-    
-  }
-
-  p{    
-    text-align:center;
-  }
-
-
-
-  @media (max-width: 768px) {
-    min-width: 300px;
-  } 
-`;
-
-const ProgressBarContainer = styled.div`
-  display: flex;
-  align-items:center;
-  gap: 10px;
-  border-radius: 10px;
-  
-`;
-
-const ProgressBar = styled.div`
-  height: 100%;
-  border-radius: 10px;
-  background-color: ${(props) => props.color};
-  min-width: 30%;
-  width: ${(props) => props.width}%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  font-weight: bold;
-  position: relative;
-  overflow: hidden;
-  padding: 2px ;
-
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.2);
-    z-index: 1;
-  }
-`;
-
-const BackButton = styled.button`
-  display: flex; 
-background-color: transparent;
-  color: white;
-
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    opacity: 0.5;
-  }
-  
-`;
-const Teste = styled.div`
-  
-    display: flex;
-    align-items:center;
-    gap:20px;
-  
-    justify-content:space-between;
- 
-`;
-
--- src/components/PokemonDetails/styles.jsx
+++ src/components/PokemonDetails/styles.jsx
@@ -2,6 +2,121 @@ import styled from 'styled-components';
 import { FaArrowLeft } from 'react-icons/fa';
 
 export const StyledArrowLeft = styled(FaArrowLeft)`
-  color: ${props=>props.theme.color}; 
-  font-size: 30px; 
+  color: ${props => props.theme.color}; 
+  font-size: 30px;   
+ `;
+
+export const Container = styled.div`
+display: flex;
+
+@media (max-width: 1024px) {
+  flex-direction: column;
+  padding: 30px;
+  align-items:center;
+  justify-content: center;
+
+  img {
+    width: 350px;     
+    padding-bottom: 30px; 
+  }
+}
+
+@media (max-width: 768px) {
+  font-size: 12px;   
+  img {
+    width: 250px;
+  }
+}
+`;
+
+ export const ContainerDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 550px;
+  gap: 20px;
+  padding: 20px;
+  box-shadow: ${props => props.theme.boxShadow};
+  background-color: ${props => props.background};
+
+  h2 {
+    font-size: 40px;
+    color: aliceblue;
+    background-color: ${props => props.color};
+    padding: 10px;
+    width: 100%;
+    text-align: center;
+    border-radius: 5px;
+  }
+
+  span {
+    background-color: ${props => props.color};
+    padding: 5px 15px;
+    color: white;
+    text-align: center;
+    border-radius: 3px;
+    font-size: 18px;
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+  }
+
+  p {
+    text-align: center;   
+      }
+
+  @media (max-width: 768px) {    
+    span { 
+    font-size: 16px;
+  }   
+  }
+`;
+
+export const ProgressBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  border-radius: 10px;
+`;
+
+export const ProgressBar = styled.div` 
+  border-radius: 10px;
+  background-color: ${(props) => props.color};
+  min-width: 10%;
+  width: ${(props) => props.width}%;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  position: relative;
+  overflow: hidden;
+  padding:  4px 2px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
+`;
+
+export const BackButton = styled.button`
+  display: flex;
+  background-color: transparent;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+export const Div = styled.div`
+  display: flex;
+  align-items: center;  
+  justify-content: space-between;
 `;
